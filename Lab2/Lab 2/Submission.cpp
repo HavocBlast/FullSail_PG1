aba9b2a9e70f58c7229b1afb2ac0a9e984713f27
Jacob Kruse <jrkruse@student.fullsail.edu>
2024-04-21 23:22:19 -0400

    Adds starting Lab 2 project

---
+++ Lab2/Lab 2/Submission.cpp
@@ -0,0 +1,86 @@
+#include <string>
+#pragma warning(disable:4244)
+
+using namespace std;
+
+class Submission {
+public:
+
+	/* Test 3
+	 * =========
+	 * Given a string, using the stoi() method, convert the string 
+	 * to an integer. Return the integer.
+	*/
+	static int Test3(string input) {
+		return 0;
+	}
+
+	/* Test 4
+	 * =========
+	 * Given a string, using the stod() method, convert the string
+	 * to a double. Return the double.
+	*/
+	static double Test4(string input) {
+		return 0;
+	}
+
+	/* Test 5
+	 * =========
+	 * Given a string, using the stof() method, convert the string
+	 * to a float. Return the float.
+	*/
+	static float Test5(string input) {
+		return 0;
+	}
+
+	/* Test 6
+	 * =========
+	 * Given a string, using the stoul() method, convert the string
+	 * to an unsigned integer. Return the unsigned integer.
+	*/
+	static unsigned int Test6(string input) {
+		return 0;
+	}
+
+	/* Test 7
+	 * =========
+	 * Given a string, using the stoull() method, convert the string
+	 * to an unsigned long. Return the unsigned long.
+	*/
+	static unsigned long Test7(string input) {
+		return 0;
+	}
+
+	/* Test 8
+	 * =========
+	 * Given a double, using a cast, convert the double
+	 * to a float. Return the float.
+	*/
+	static float Test8(double input) {
+		return 0;
+	}
+
+	/* Test 9
+	 * =========
+	 * Given a float, using a cast, convert the float
+	 * to an integer. Return the integer.
+	*/
+	static int Test9(float input) {
+		return 0;
+	}
+
+	/* Test 10
+	 * =========
+	 * Given an integer, convert the integer
+	 * to a string using the to_string() method.
+	 *
+	 * Return the string.
+	 *
+	 * References:
+	 * - https://cplusplus.com/reference/string/to_string/
+	 * - https://www.geeksforgeeks.org/stdto_string-in-cpp/
+	*/
+	static string Test10(int input) {
+		return "";
+	}
+};
